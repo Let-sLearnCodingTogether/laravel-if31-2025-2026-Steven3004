345fa9de3793117753960429a9be79bfa3492064
Steven <stevenjunius30@gmail.com>
2025-09-18 17:30:05 +0700

    update 18/09/2025

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request) {
+
+    }
+    public function register() {
+
+    }
+    public function logout() {
+}
+}
